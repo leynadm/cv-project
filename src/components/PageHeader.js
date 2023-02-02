dfa78afe4dbd23b5cb472bee49273a76bf36a291
Leynad <matei_daniel@outlook.com>
2023-02-02 16:16:32 +0100

    Remove not needed packages

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -2,9 +2,6 @@ import React from "react";
 import { Component } from "react";
 import "../styles/PageHeader.css";
 import reactLogo from "../images/logo.svg";
-import jsPDF from "jspdf";
-import * as htmlToImage from 'html-to-image';
-import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
 
 class PageHeader extends Component {
 
@@ -13,36 +10,6 @@ class PageHeader extends Component {
     this.props.handlePreview();
   };
 
-  generatePDF = () => {
-
-    const report = new jsPDF('portrait','pt','a4');
-    
-    report.html(document.querySelector('.CV-page')).then(() => {
-        report.save('report.pdf');
-    });
-  
-  }
-
-  generateImage =() => htmlToImage.toPng(document.querySelector('.CV-page'), { quality: 0.95 })
-  .then(function (dataUrl) {
-    var link = document.createElement('a');
-    link.download = 'my-image-name.jpeg';
-    const pdf = new jsPDF();          
-    pdf.addImage(dataUrl, 'PNG', 0, 0);
-    pdf.save("download.pdf"); 
-  });
-
-  generatePDFImage =() =>  htmlToImage.toPng(document.querySelector('.CV-page'), { quality: 0.95 })
-  .then(function (dataUrl) {
-    var link = document.createElement('a');
-    link.download = 'my-image-name.jpeg';
-    const pdf = new jsPDF();
-    const imgProps= pdf.getImageProperties(dataUrl);
-    const pdfWidth = pdf.internal.pageSize.getWidth();
-    const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-    pdf.addImage(dataUrl, 'PNG', 0, 0,pdfWidth, pdfHeight);
-    pdf.save("download.pdf"); 
-  });
 
   render() {
     return (
