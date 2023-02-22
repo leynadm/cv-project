d21ca07caf8c28b7b76cee4e0be2794a9586ab4b
Leynad <matei_daniel@outlook.com>
2023-02-22 17:29:48 +0100

    Update before pulling on different PC

--- src/components/CV-One.js
+++ src/components/CV-One.js
@@ -11,18 +11,18 @@ import CVOneDescription from "./CV-One-Description";
 const CVOne = forwardRef((props, ref) => {
   const { currentClass, textareaClass } = props;
   return (
-    <div ref={ref} className="CV-one-page">
+    <div ref={ref} className="cv-one-page">
       <CVOneProfile />
       <CVOneDescription />
-      <div className="CV-one-sides">
-        <div className="CV-one-left-side">
+      <div className="cv-one-sides">
+        <div className="cv-one-left-side">
           <CVOnePersonalInfo currentClass={currentClass} />
           <CVOneDigitalSkills currentClass={currentClass} />
           <CVOneLanguages currentClass={currentClass} />
           <CVOneEducation currentClass={currentClass} />   
         </div>
         
-        <div className="CV-one-right-side">
+        <div className="cv-one-right-side">
           <CVOneProfExp
             currentClass={currentClass}
             textareaClass={textareaClass}
